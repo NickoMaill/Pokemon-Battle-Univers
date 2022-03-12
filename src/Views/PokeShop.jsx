b0853930c36bab2719acc57d54b5874949c1c542
Nicko <nickos@iMac.local>
2022-03-12 01:27:00 +0100

    set basket function

--- src/Views/PokeShop.jsx
+++ src/Views/PokeShop.jsx
@@ -1,5 +1,45 @@
+import { useContext, useEffect, useState } from "react"
+import { Context } from "../Context/AppContext"
+
+import Catalog from "../data/marketCatalog.json"
+
 export default function PokeShop() {
-    return(
-        <h1>Pokeshop</h1>
+
+    const stateContext = useContext(Context);
+    const [basket, setBasket] = useState([]);
+    const [counts, setCounts] = useState({});
+    const tempArray = [] // provisory solution
+
+    const addItem = (newItem) => {
+        setBasket(prevBasket => [...prevBasket, newItem]);
+    }
+
+    useEffect(() => {
+        const count = {};
+        basket.forEach((item) => {
+            count[item] = (count[item] || 0) + 1;
+            setCounts(count);
+        });
+
+    }, [basket]);
+
+    return (
+
+        <>
+            <h1>Pokeshop</h1>
+            <button onClick={() => addItem(Catalog[0].productName)}>add item</button>
+            <button onClick={() => addItem(Catalog[1].productName)}>add item</button>
+            <div>
+                <ul>
+                    {basket.map((item, i) => {
+                        if (tempArray.includes(item)) {
+                        } else {
+                            tempArray.push(item)
+                            return <li key={i}>{item} x{counts[item]}</li>
+                        }
+                    })}
+                </ul>
+            </div>
+        </>
     )
 }
